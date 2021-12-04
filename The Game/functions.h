ca5db97e3a6f1b454e3cd64e443f3d6cc7481876
Romankorobeinikov <korobeinikov.rv@phystech.edu>
2021-12-04 16:00:42 +0300

    интерфейс меню загрузки

---
+++ The Game/functions.h
@@ -0,0 +1,40 @@
+using namespace std;
+
+int select_pun(int maxi)
+{
+	btn = getch();
+	switch (btn) {
+	case (72): //('w' || 'W' || 72):
+		if (pun==1) {
+			kur[0] = ' ';
+			pun = maxi;
+			kur[maxi-1] = '>';
+			return 0;
+		} else {
+			kur[pun-1] = ' ';
+			kur[pun-2] = '>';
+			pun--;
+			return 0;
+		}
+	break;
+	case (80): //('s' || 'S' || 80):
+		if (pun==maxi) {
+			kur[maxi-1] = ' ';
+			pun = 1;
+			kur[0] = '>';
+			return 0;
+		} else {
+			kur[pun-1] = ' ';
+			kur[pun] = '>';
+			pun++;
+			return 0;
+		}
+	break;
+	case (13):
+		kur[pun-1] = ' ';
+		return pun;
+	break;
+	default:
+		return 0;
+	}
+}

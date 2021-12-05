f483d85e28372302bd5e5b2b21e7aef8a8885547
Romankorobeinikov <korobeinikov.rv@phystech.edu>
2021-12-05 13:14:35 +0300

    Сохранение сложности

--- The Game/functions.h
+++ The Game/functions.h
@@ -102,7 +102,7 @@ void save_s()
 	save<<p_name<<endl<<gold<<endl<<p_at<<endl<<p_hp<<endl<<p_ar<<endl<<p_lv<<endl<<p_xp<<endl<<lvl_loc
 	<<endl<<tren<<endl<<victory<<endl<<enemy<<endl<<enemy_cof<<endl<<loc_score<<endl<<all_gold<<endl
 	<<bodies<<endl<<ydar<<endl<<promah<<endl<<proid<<endl<<win<<endl<<lose<<endl<<death<<endl
-	<<att_id<<endl<<arm_id<<endl<<item_id<<endl<<arena_lock<<endl<<info;
+	<<att_id<<endl<<arm_id<<endl<<item_id<<endl<<arena_lock<<endl<<info<<endl<<difficulty<<endl;
 	save.close();
     delay(1000);
     printf("Успешно сохранено!\n");
@@ -118,7 +118,7 @@ void load_s()
 		system("cls");
 		printf("Загрузка...");
 		load>>p_name>>gold>>p_at>>p_hp>>p_ar>>p_lv>>p_xp>>lvl_loc>>tren>>victory>>enemy>>enemy_cof>>loc_score>>all_gold>>bodies>>
-		ydar>>promah>>proid>>win>>lose>>death>>att_id>>arm_id>>item_id>>arena_lock>>info;
+		ydar>>promah>>proid>>win>>lose>>death>>att_id>>arm_id>>item_id>>arena_lock>>info>>difficulty;
 		load.close();
 		delay(1000);
 	} else {
